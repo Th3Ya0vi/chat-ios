c25ee0168340c39d1a56c9b1f505f6f437bb0e20
hongyu <hongyu@hotoa.com>
2014-03-14 16:10:07 +0800

    release 1.5.0
    增加file和geolocation的插件

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -221,6 +221,34 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.inappbrowser/www/InAppBrowser.js",
         "id": "org.apache.cordova.inappbrowser.InAppBrowser",
         "clobbers": [
@@ -256,6 +284,7 @@ module.exports.metadata =
     "org.apache.cordova.contacts": "0.2.7",
     "org.apache.cordova.dialogs": "0.2.4",
     "org.apache.cordova.file": "1.0.1",
+    "org.apache.cordova.geolocation": "0.3.6",
     "org.apache.cordova.inappbrowser": "0.3.0",
     "org.apache.cordova.splashscreen": "0.2.6",
     "org.apache.cordova.statusbar": "0.1.3",
